ccd8efc6944008efc86f335d4bd5baa19af3e24f
7651958760 <151062336+7651958760@users.noreply.github.com>
2024-10-31 13:09:38 +0000

    datatype

---
+++ on_js/datatype.js
@@ -0,0 +1,24 @@
+"user script"
+console.log(3+3)
+let name="himanshu"
+let age=20
+let isLoggedIn=false
+
+//datatpes
+/*
+number-->2 power 53 range
+bigint--> in trading and stock market
+string-->prfer in doule quotes
+boollean-->true or false
+null-->standalone value
+undefined-->not assigned value
+symbol-->uniqueness
+
+
+*/
+//lets check object-->
+console.log(typeof "himanshu");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
